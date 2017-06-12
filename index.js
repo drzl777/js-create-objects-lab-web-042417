c3df0f8e70bf2635af17ebd630b75856cbfcc821
drzl777 <andrew.zheng725@gmail.com>
2017-06-12 12:30:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 'use strict';
+// var Dog = function (name, age, breed) {
+//   this.name = name
+// }
+
+var Dog = function(name,breed,age) {
+  this.name = name
+  this.breed = breed
+  this.age = age
+}
+
+
+var Cat = function(name,breed,age) {
+  this.name = name
+  this.breed = breed
+  this.age = age
+}
+
+var Ferret = function(name,age,foodRequirements) {
+  this.name = name
+  this.age = age
+  this.foodRequirements = foodRequirements
+}
+
+var Bird = function(name, breed) {
+  this.name = name
+  this.breed = breed
+}
+
+var Fish = function(name,species, waterConditions) {
+  this.name = name
+  this.species = species
+  this.waterConditions = waterConditions
+}
